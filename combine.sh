356e148ba8e606fd6ec5a5403132947a04501d62
Xunliang Liu <xunliangliu@email.arizona.edu>
2015-02-22 12:22:31 -0700

    combine file

--- combine.sh
+++ combine.sh
@@ -9,9 +9,11 @@ OUTPUT_FILE="output/combined_gapMinder.tsv"
 echo Building combined country data file
 cat data/clean/country.cc.txt > $OUTPUT_FILE
 
+#move countries file so we can ge everything but the country header file
 mv -v data/clean/country.cc.txt data/clean/country.cc.processed
 sleep 2
 
+#
 cat data/clean/*.cc.txt 
 sleep 2
 cat data/clean/*.cc.txt >> $OUTPUT_FILE
